6047830d7831340d1d3f52a371fa3c04b2e910b4
Kiho Okazawa <kokazawa@lcs.on.ca>
2023-04-04 09:14:18 -0400

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Kiho Okazawa on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
